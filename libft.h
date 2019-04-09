4f5c38dad28c0cc89ed9e3c5c0425e29266261da
Arianna Davis <adavis@at-n7.21-school.ru>
2019-04-09 15:31:03 +0300

    Remade header file - first & bonus part only

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: adavis <adavis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/23 18:15:19 by adavis            #+#    #+#             */
-/*   Updated: 2019/04/08 18:13:58 by adavis           ###   ########.fr       */
+/*   Updated: 2019/04/09 15:29:40 by adavis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,38 @@
 # include <string.h>
 # include <libc.h>
 
+/*			Part 1 - Libc functions			*/
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, void *src, size_t n);
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
-char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
-int		ft_atoi(const char *str);
+char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strncpy(char *dst, const char *src, size_t len);
+char	*ft_strcat(char *restrict s1, const char *restrict s2);
+char	*ft_strncat(char *restrict s1, const char *restrict s2, size_t n);
+/*		size_t	ft_strlcat(char *dst, const char *src, size_t size);	!TBD	*/
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strstr(const char *haystack, const char *needle);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_atoi(const char *str);
 int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+
+/*			Bonus part			*/
 int		ft_islower(int c);
 int		ft_isupper(int c);
-int		ft_isdigit(int c);
-char	*ft_strnew(size_t size);
-void	ft_putchar(char c);
-char	*ft_strdup(const char *s1);
-void	ft_putchar_fd(char c, int fd);
-int		ft_isspace(char c);
-void	*ft_memcpy(void *dst, void *src, size_t n);
-char	*ft_strncpy(char *dst, const char *src, size_t len);
-void	ft_putstr(char const *s);
-void	ft_strrev(char *str);
 
 #endif
