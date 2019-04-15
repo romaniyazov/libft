98e7dd247763b9006bcda3a72027aee14c55affc
Arianna Davis <adavis@vo-k2.21-school.ru>
2019-04-15 22:17:52 +0300

    All tests passed! ft_strncmp works!

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: adavis <adavis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/27 19:49:33 by adavis            #+#    #+#             */
-/*   Updated: 2019/04/09 16:46:07 by adavis           ###   ########.fr       */
+/*   Updated: 2019/04/15 22:17:13 by adavis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	if (s1[0] == '\0' && s2[0] == '\0')
+	if ((s1[0] == '\0' && s2[0] == '\0') || n == 0)
 		return (0);
 	if (s2[0] == '\0')
 		return (1);
 	if (s1[0] == '\0')
 		return (-1);
-	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
+	while (s1 && s2 && i < n)
 	{
 		if (s1[i] != s2[i])
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
